0df47d3ca81f1ce8c66fbe6c9dee6d51111349ab
Shah <shoaibshah1255@gmail.com>
2025-01-12 16:49:41 -0500

    creted patches and after install

--- educators/install.py
+++ educators/install.py
@@ -1,38 +1,44 @@
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
-
-
-
 def after_install():
-	create_custom_fields(get_custom_fields())
-	
-
-
+    # Add custom fields defined in get_custom_fields
+    create_custom_fields(get_custom_fields())
     
+    # Update order_type options in Quotation
+    create_custom_fields(update_quotation_order_type())
+
 def get_custom_fields():
-	"""Education specific custom fields that needs to be added to the Sales Invoice DocType & Quotation Doctype."""
-	return {
-		"Sales Invoice": [
-			{
-				"fieldname": "custom_student_name",
-				"fieldtype": "Link",
-				"label": "Student Name",
-				"options": "Student",
-				"insert_after": "naming_series",
-			},
+    """Define custom fields for Sales Invoice and Quotation doctypes."""
+    return {
+        "Sales Invoice": [
+            {
+                "fieldname": "custom_student_name",
+                "fieldtype": "Link",
+                "label": "Student Name",
+                "options": "Student",
+                "insert_after": "naming_series",
+            },
         ],
-      "Quotation": [
-    {
-        "fieldname": "custom_order_type",
-        "fieldtype": "Select",
-        "label": "Order Type",
-        "options": "Property Sell\nProperty Rent\nProperty Maintenance\nSales\nMaintenance\nShopping Cart",
-        "insert_after": "column_break1",
-		"default": "Property Sell",
-        "reqd": 1,
-        "print_hide": 1,
-        "in_standard_filter": 1
-    },
-  ],
- }
+    }
+
+def update_quotation_order_type():
+    """Update order_type options in the Quotation doctype."""
+    if frappe.db.exists('DocType', 'Quotation'):
+        doc = frappe.get_doc('DocType', 'Quotation')
+        for field in doc.fields:
+            if field.fieldname == 'order_type':
+                
+                current_options = field.options.split('\n')
+                
+                
+                new_options = ['Rent', 'Property Sell', 'Property Maintenance']
+                
+               
+                updated_options = list(set(current_options + new_options))
+                
+              
+                field.options = '\n'.join(updated_options)
+                doc.save()
+
+                frappe.clear_cache(doctype='Quotation')
--- educators/patches/v1_0/quotation_order_type_field.py
+++ educators/patches/v1_0/quotation_order_type_field.py
@@ -1,7 +1,7 @@
 import frappe
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
-from educators.install import get_custom_fields
+from educators.install import update_quotation_order_type
 
 
 def execute():
-	create_custom_fields(get_custom_fields())
+	create_custom_fields(update_quotation_order_type())
